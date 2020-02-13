fa92cfccf50d941e1c46dba87cf47349f63143de
Paul Cuzner <pcuzner@redhat.com>
2020-02-13 15:35:46 +1300

    web: update masthead to show status metadata
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- react/app/src/components/masthead.jsx
+++ react/app/src/components/masthead.jsx
@@ -7,11 +7,17 @@ import '../app.scss';
 export class MastHead extends React.Component {
     constructor(props) {
         super(props);
+        this.interval = 0;
         this.state = {
-            version: 1
+            task_active: false,
+            tasks_queued: 0,
+            task_type: 'N/A',
+            target: '',
+            run_time: 0
         };
     };
-    componentDidMount() {
+
+    getStatus() {
         fetch("http://localhost:8080/api/status")
           .then((response) => {
               console.debug("status fetch : ", response.status);
@@ -22,19 +28,56 @@ export class MastHead extends React.Component {
               })
           .then((status) => {
               /* Happy path */
-              console.log(JSON.stringify(status));
+              state = status.data;
+              this.setState(state);
+            //   console.log(JSON.stringify(state));
           })
           .catch((error) => {
               console.error("Error:", error);
+              console.error("killing interval based status checking");
+              clearInterval(this.interval);
           });
+
+    }
+
+    intervalHandler = () => {
+        this.getStatus();
+    }
+
+    componentDidMount() {
+        console.log("starting interval based call to the /status API endpoint");
+        this.interval = setInterval(this.intervalHandler, 5000);
     }
 
     render() {
         return (
             <div id="masthead">
-                <h1>LoadGen</h1>
-            </div>
+                <div>
+                    <div className="page-heading">LoadGen</div>
+                    <ServiceState state={this.state}/>
+                </div>
+                <div style={{clear: 'both'}}/>
+            </div>    
         );
+    }
+}
+
+class ServiceState extends React.Component {
+    constructor(props) {
+        super(props);
+        self.state = {
+            dummy: false
+        };
+    }
+    render() {
+        let taskText = this.props.state.task_active ? "Yes" : "no";
+        return (
+            <div className="status-area">
+                <div className="float-right status-spacing">Job Active:{taskText}</div>
+                <div className="float-right status-spacing">Queued:{this.props.state.tasks_queued}</div>
+                <div className="float-right status-spacing">Platform:{this.props.state.target}</div>
+            </div>
+        )
     }
 }
 
