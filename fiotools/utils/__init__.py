1edc42325d413a42c7bc992724b8680be4bad7f7
Paul Cuzner <pcuzner@redhat.com>
2022-01-09 19:34:54 +1300

    core: Add valid_job_id to utils module
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- fiotools/utils/__init__.py
+++ fiotools/utils/__init__.py
@@ -1,1 +1,1 @@
-from .utils import rfile, get_pid_file, port_in_use, generate_fio_profile  # NOQA:F401
+from .utils import rfile, get_pid_file, port_in_use, generate_fio_profile, valid_job_id  # NOQA:F401
