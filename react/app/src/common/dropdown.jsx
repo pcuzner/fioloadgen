bca5bbc45abf802d6e5abe0059825fe4523930e5
Paul Cuzner <pcuzner@redhat.com>
2022-01-09 19:38:42 +1300

    UI: Add drop down component
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

---
+++ react/app/src/common/dropdown.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import '../app.scss';
+
+
+export class DropDownOptions extends React.Component {
+    //
+    // Basic drop down component inspired by w3schools
+    constructor(props) {
+        super(props);
+        this.state = {
+            contentVisible: false
+        };
+    }
+
+    toggleOptions = () => {
+        this.setState({
+            contentVisible: !this.state.contentVisible
+        });
+
+    }
+
+    optionSelected = (opt) => {
+        console.debug("dropdown:optionSelected: returning " + opt);
+        this.setState({
+            contentVisible: false
+        });
+        this.props.callback(opt);
+    } 
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (JSON.stringify(nextProps.optionMap) !== JSON.stringify(this.props.optionMap)) {
+            return true;
+        }
+        if (nextProps.disabled !== this.props.disabled) {
+            return true;
+        }
+        if (this.state.contentVisible !== nextState.contentVisible) {
+            return true;
+        }
+        return false;
+    }
+    
+
+    render() {
+        console.debug('dropdown:render: processing');
+        let options;
+        if (this.state.contentVisible) {
+            options = "dropdown-content dropdown-content-visible";
+        } else {
+            options = "dropdown-content dropdown-content-hidden";
+        }
+        let dropDownOptions;
+        if (this.state.contentVisible){
+            dropDownOptions = this.props.optionMap.map((option, i) => {
+                return (
+                    <a href="#" key={i} onClick={() => {this.optionSelected(option.name)}}>{option.text}</a>
+                );
+            });
+
+        } else {
+            dropDownOptions = (<div />);
+        }
+        return (
+            <div className="dropdown">
+                <button disabled={this.props.disabled} onClick={() => {this.toggleOptions()}} className="btn btn-secondary">{this.props.label}<span className="caret"/></button>
+                <div className={options}>
+                    {dropDownOptions}
+                </div>
+            </div>
+        );
+    }
+}
